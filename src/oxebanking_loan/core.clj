9af1e32c27c095fd152606031f29d840204da831
João Lucas <jloc@ic.ufal.br>
2024-10-22 01:09:40 -0300

    🚀 feat: creating api's and adding schemas

--- src/oxebanking_loan/core.clj
+++ src/oxebanking_loan/core.clj
@@ -9,7 +9,7 @@
   (:gen-class))
 
 (def db-spec {:dbtype "postgresql"
-              :dbname "clojure_testing"
+              :dbname "oxebanking"
               :user "postgres"
               :password "root"
               :host "localhost"
@@ -22,48 +22,94 @@
       (content-type "application/json")
       (status (or status-code 200))))
 
-;; CRUD operations
 
-(defn create-user [user-data]
-  (sql/insert! datasource :users user-data))
+(defn create-loan-request [loan-data]
+  (sql/insert! datasource :loanRequests loan-data))
 
-(defn get-users []
-  (sql/query datasource ["SELECT * FROM users"]))
+(defn get-loan-requests []
+  (sql/query datasource ["SELECT * FROM loanRequests"]))
 
-(defn get-user [id]
-  (sql/get-by-id datasource :users id))
+(defn get-loan-request [id]
+  (sql/get-by-id datasource :loanRequests id))
 
-(defn update-user [id user-data]
-  (sql/update! datasource :users user-data {:id id}))
+(defn update-loan-request [id loan-data]
+  (sql/update! datasource :loanRequests loan-data {:id id}))
 
-(defn delete-user [id]
-  (sql/delete! datasource :users {:id id}))
+(defn delete-loan-request [id]
+  (sql/delete! datasource :loanRequests {:id id}))
+
+
+(defn create-loan [loan-data]
+  (sql/insert! datasource :loans loan-data))
+
+(defn get-loans []
+  (sql/query datasource ["SELECT * FROM loans"]))
+
+(defn get-loan [id]
+  (sql/get-by-id datasource :loans id))
+
+
+(defn create-loan-payment [payment-data]
+  (sql/insert! datasource :loanPayments payment-data))
+
+(defn get-loan-payments [loan-id]
+  (sql/query datasource ["SELECT * FROM loanPayments WHERE loanId = ?" loan-id]))
 
 (defroutes app-routes
-  (GET "/users" []
-    (let [users (get-users)]
-      (json-response {:users users})))
 
-  (GET "/users/:id" [id]
-    (let [user (get-user (Integer/parseInt id))]
-      (if user
-        (json-response user)
-        (json-response {:error "User not found"} 404))))
-
-  (POST "/users" {body :body}
-    (let [user-data (json/parse-string (slurp body) true)
-          result (create-user user-data)]
+  ;; Loan Requests
+  (POST "/loans/request" {body :body}
+    (let [loan-data (json/parse-string (slurp body) true)
+          result (create-loan-request loan-data)]
       (json-response {:result result} 201)))
 
-  (PUT "/users/:id" {body :body params :params}
-    (let [user-id (Integer/parseInt (:id params))
-          user-data (json/parse-string (slurp body) true)]
-      (update-user user-id user-data)
-      (json-response {:message "User updated"})))
+  (GET "/loans/request" []
+    (let [requests (get-loan-requests)]
+      (json-response {:requests requests})))
 
-  (DELETE "/users/:id" [id]
-    (delete-user (Integer/parseInt id))
-    (json-response {:message "User deleted"})))
+  (GET "/loans/request/:id" [id]
+    (let [request (get-loan-request (Integer/parseInt id))]
+      (if request
+        (json-response request)
+        (json-response {:error "Loan request not found"} 404))))
 
-(defn -main []
-  (run-jetty app-routes {:port 3000 :join? false}))
+  (PUT "/loans/request/:id" {body :body params :params}
+    (let [request-id (Integer/parseInt (:id params))
+          loan-data (json/parse-string (slurp body) true)]
+      (update-loan-request request-id loan-data)
+      (json-response {:message "Loan request updated"})))
+
+  (DELETE "/loans/request/:id" [id]
+    (delete-loan-request (Integer/parseInt id))
+    (json-response {:message "Loan request deleted"}))
+
+  ;; Loans
+  (POST "/loans" {body :body}
+    (let [loan-data (json/parse-string (slurp body) true)
+          result (create-loan loan-data)]
+      (json-response {:result result} 201)))
+
+  (GET "/loans" []
+    (let [loans (get-loans)]
+      (json-response {:loans loans})))
+
+  (GET "/loans/:id" [id]
+    (let [loan (get-loan (Integer/parseInt id))]
+      (if loan
+        (json-response loan)
+        (json-response {:error "Loan not found"} 404))))
+
+  ;; Loan Payments
+  (POST "/loans/:id/payments" {body :body params :params}
+    (let [payment-data (json/parse-string (slurp body) true)
+          loan-id (Integer/parseInt (:id params))
+          result (create-loan-payment (assoc payment-data :loanId loan-id))]
+      (json-response {:result result} 201)))
+
+  (GET "/loans/:id/payments" [id]
+    (let [payments (get-loan-payments (Integer/parseInt id))]
+      (json-response {:payments payments})))
+
+
+  (defn -main []
+    (run-jetty app-routes {:port 3000 :join? false})))
